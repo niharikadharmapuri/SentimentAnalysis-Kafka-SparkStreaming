062782aacfd088a2af8d932bbf8f3f75b7401a9b
niharikadharmapuri <niharika.dharmapuri@gmail.com>
2018-10-09 12:21:39 -0500

    basic txt

--- connector.py
+++
@@ -1,4 +0,0 @@
-Use case:
-use built in Kafka connectors to  monitor a file(access_log.txt.txt) and publish new lines on that file to a 
-given Kafka topic that then get written out to some other file(logout.txt) somewhere else.
-
